8aae4a6b28660fd91b063e03ec631062427a7b6f
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-04-04 20:31:00 -0300

    Applying a simples style to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-         runApp( const MyApp()  );
+         runApp(  new GiftApp()  ); // antes: const MyApp()
 }
 
 
 class GiftApp extends StatelessWidget {
 
         @override
-        Widget build(BuildContext context) {
+        Widget build(BuildContext context) { // funcao q constroi seu wiget
               return MaterialApp(
                         title: "GifFlu",
-                        home: new Scaffold(  // o new eh opcional
+                        home: new Scaffold( //home eh a tag q espera uma pagina representada por uma classe Widget
                                 appBar: new AppBar(
-                                      title: Text("Titulo aqui"),
+                                      title: Text("Hello from demo!!"),
                                 ),
-                        )
+                        ),
+                      theme: ThemeData(  // o new eh opcional
+                           primarySwatch: Colors.indigo,
+                           visualDensity: VisualDensity.adaptivePlatformDensity
+                      )
               );
               //  throw UnimplementedError();
         }
-
 }
-
-
 
 
 
@@ -37,62 +38,42 @@ class MyApp extends StatelessWidget {
             return MaterialApp(
                     title: 'Flutter Demo',
                     theme: ThemeData(
-                            // This is the theme of your application.
-                            //
-                            // Try running your application with "flutter run". You'll see the
-                            // application has a blue toolbar. Then, without quitting the app, try
-                            // changing the primarySwatch below to Colors.green and then invoke
-                            // "hot reload" (press "r" in the console where you ran "flutter run",
-                            // or simply save your changes to "hot reload" in a Flutter IDE).
-                            // Notice that the counter didn't reset back to zero; the application
-                            // is not restarted.
                             primarySwatch: Colors.blue,
                     ),
-                    home: const MyHomePage(title: 'Flutter Demo Home Page'),
+                    home: const MyHomePage(title: "testando"),
+                          // use 'const' para somente construtores declarados com 'const'. Ele eh opcional
             );
           }
 }
 
-class MyHomePage extends StatefulWidget {
-        const  MyHomePage({super.key, required this.title});
 
-        // This widget is the home page of your application. It is stateful, meaning
-        // that it has a State object (defined below) that contains fields that affect
-        // how it looks.
 
-        // This class is the configuration for the state. It holds the values (in this
-        // case the title) provided by the parent (in this case the App widget) and
-        // used by the build method of the State. Fields in a Widget subclass are
-        // always marked "final".
-
-        final String title;
+class MyHomePage extends StatefulWidget {  // widget com estado mutavel
+        const MyHomePage({ // parametros dentro de chaves sao opcionais, exceto aqueles com "required"
+                    super.key, // recebe e ja envia a key para o construtor
+                    required this.title // recebe e ja atribui o valor do parametro para a propriedade interna "title". "this.title" pode ter o "String" na frente, mas ele eh detectado por inferencia
+        });
+       final  String title;
+        // toda classe com propriedades final devem ter um construtor const !!
 
         @override
         State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-        setState(() {
-            // This call to setState tells the Flutter framework that something has
-            // changed in this State, which causes it to rerun the build method below
-            // so that the display can reflect the updated values. If we changed
-            // _counter without calling setState(), then the build method would not be
-            // called again, and so nothing would appear to happen.
-            _counter++;
-        });
-  }
+
+
+class _MyHomePageState extends State<MyHomePage> {
+        int _counter = 0;
+
+        void _incrementCounter() {
+              setState(() {
+                      _counter++;
+              });
+        }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
