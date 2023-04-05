129ea8f8d5abecb82d39950b79670bde1c780a00
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-04-05 00:29:48 -0300

    Applying a simples style to main screen - part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-         runApp(  new GiftApp()  ); // antes: const MyApp()
+         runApp(  const  MyApp()  ); // antes: const MyApp()
 }
 
 
@@ -11,8 +11,8 @@ class GiftApp extends StatelessWidget {
         Widget build(BuildContext context) { // funcao q constroi seu wiget
               return MaterialApp(
                         title: "GifFlu",
-                        home: new Scaffold( //home eh a tag q espera uma pagina representada por uma classe Widget
-                                appBar: new AppBar(
+                        home:  Scaffold( //home eh a tag q espera uma pagina representada por uma classe Widget
+                                appBar:  AppBar( // o instanceamento de Widgets/classes podem ser feitos sem o "new"
                                       title: Text("Hello from demo!!"),
                                 ),
                         ),
@@ -28,19 +28,19 @@ class GiftApp extends StatelessWidget {
 
 
 
-
+// classe com construtor const  q inicialmente eh usada pelo "runApp()"
 class MyApp extends StatelessWidget {
-          const MyApp({super.key});
+          const MyApp({super.key}); // construtores "const" sao opcionais !!
 
-          // This widget is the root of your application.
           @override
-          Widget build(BuildContext context) {
+          Widget build ( BuildContext context ) {
             return MaterialApp(
                     title: 'Flutter Demo',
                     theme: ThemeData(
-                            primarySwatch: Colors.blue,
+                            primarySwatch: Colors.orange,
+                            secondaryHeaderColor: Colors.black
                     ),
-                    home: const MyHomePage(title: "testando"),
+                    home: const MyHomePage(title: "Meu primeiro app"), // neste exemplo o home eh uma outra classe, mas tbm pode ser configurado sem outra classe, assim como na classe "GiftApp"
                           // use 'const' para somente construtores declarados com 'const'. Ele eh opcional
             );
           }
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
 
 
 class MyHomePage extends StatefulWidget {  // widget com estado mutavel
-        const MyHomePage({ // parametros dentro de chaves sao opcionais, exceto aqueles com "required"
+        const  MyHomePage({ // parametros dentro de chaves sao opcionais, exceto aqueles com "required"
                     super.key, // recebe e ja envia a key para o construtor
                     required this.title // recebe e ja atribui o valor do parametro para a propriedade interna "title". "this.title" pode ter o "String" na frente, mas ele eh detectado por inferencia
         });
@@ -67,54 +67,45 @@ class _MyHomePageState extends State<MyHomePage> {
         int _counter = 0;
 
         void _incrementCounter() {
-              setState(() {
-                      _counter++;
+              setState(() { // passando funcao lambda/callback para o setState
+                      this._counter++; // aqui o 'this' nao eh necessario
               });
         }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        // a funcao "build" abaixo funciona como um renderizador do State
+        @override
+        Widget build(BuildContext context) { //chamado toda vez que o estado setState() eh alterado!!
+                return Scaffold(
+                          appBar: AppBar(
+                                  centerTitle: true,
+                                  toolbarTextStyle: const TextStyle(
+                                        color:  Color.fromARGB( 102, 254, 0, 0)
+                                  ),
+                                  title: Text(widget.title),   // o "widget" eh uma propriedade exclusiva da classe State e a propriedade "title" foi adquirida atraves do State<MyHomePage>
+                          ),
+                          body: Center(
+
+                                child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                                  const Text(
+                                                        'Voce apertou o botao N vezes: ',
+                                                  ),
+                                                  Text(
+                                                          '$_counter',
+                                                          style: Theme.of(context).textTheme.headlineMedium,
+                                                          textAlign: TextAlign.left,
+                                                  ),
+                                        ],
+                                ),
+                          ),
+                          floatingActionButton: FloatingActionButton(
+                                  onPressed: this._incrementCounter, // aqui o 'this' eh opcional
+                                  tooltip: 'Increment',
+                                  child: const Icon(Icons.add),
+                          ),
+                          floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+
+                );
+        }
 }
