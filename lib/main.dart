76ee16faac7b9a74e18a173e2998dc6f4505cde6
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-04-05 23:56:41 -0300

    changing colors

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,8 @@ class MyApp extends StatelessWidget {
             return MaterialApp(
                     title: 'Flutter Demo',
                     theme: ThemeData(
-                            primarySwatch: Colors.orange,
-                            secondaryHeaderColor: Colors.black
+                            primarySwatch: null ,//Colors.orange,
+                            secondaryHeaderColor:  null, //Colors.black
                     ),
                     home: const MyHomePage(title: "Meu primeiro app"), // neste exemplo o home eh uma outra classe, mas tbm pode ser configurado sem outra classe, assim como na classe "GiftApp"
                           // use 'const' para somente construtores declarados com 'const'. Ele eh opcional
@@ -47,24 +47,24 @@ class MyApp extends StatelessWidget {
 }
 
 
-
+ // componente que representa uma pagina com estado dinamico, o qual vai ser gerenciado pela classe "_MyHomePageState"
 class MyHomePage extends StatefulWidget {  // widget com estado mutavel
-        const  MyHomePage({ // parametros dentro de chaves sao opcionais, exceto aqueles com "required"
+          const MyHomePage({ // parametros dentro de chaves sao opcionais, exceto aqueles com "required"
                     super.key, // recebe e ja envia a key para o construtor
                     required this.title // recebe e ja atribui o valor do parametro para a propriedade interna "title". "this.title" pode ter o "String" na frente, mas ele eh detectado por inferencia
         });
-       final  String title;
+         final String title;
         // toda classe com propriedades final devem ter um construtor const !!
 
         @override
-        State<MyHomePage> createState() => _MyHomePageState();
+        State<MyHomePage> createState() {
+               return _MyHomePageState();// retornando a classe estado com interface do estado
+        }
 }
 
-
-
-
+  // componente que representa o estado da pagina  "MyHomePage"
 class _MyHomePageState extends State<MyHomePage> {
-        int _counter = 0;
+        int _counter = 0; // variavel pertecente a interface (build()) do State
 
         void _incrementCounter() {
               setState(() { // passando funcao lambda/callback para o setState
@@ -74,14 +74,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
         // a funcao "build" abaixo funciona como um renderizador do State
         @override
-        Widget build(BuildContext context) { //chamado toda vez que o estado setState() eh alterado!!
+        Widget build( BuildContext context ) { //chamado toda vez que o estado setState() eh alterado!!
                 return Scaffold(
+                         backgroundColor: Colors.deepPurple,
                           appBar: AppBar(
                                   centerTitle: true,
+                                  toolbarHeight: 60.2,
                                   toolbarTextStyle: const TextStyle(
-                                        color:  Color.fromARGB( 102, 254, 0, 0)
+                                        color:  Color.fromARGB( 1, 254, 0, 0)
                                   ),
-                                  title: Text(widget.title),   // o "widget" eh uma propriedade exclusiva da classe State e a propriedade "title" foi adquirida atraves do State<MyHomePage>
+                                  title:  Text( super.widget.title ),   // o "widget" eh uma propriedade exclusiva da classe State e a propriedade "title" foi adquirida atraves do State<MyHomePage>
                           ),
                           body: Center(
 
@@ -90,11 +92,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                         children: <Widget>[
                                                   const Text(
                                                         'Voce apertou o botao N vezes: ',
+                                                        style: TextStyle( color: Colors.white),
                                                   ),
                                                   Text(
-                                                          '$_counter',
+                                                          'número atual $_counter',
                                                           style: Theme.of(context).textTheme.headlineMedium,
-                                                          textAlign: TextAlign.left,
                                                   ),
                                         ],
                                 ),
