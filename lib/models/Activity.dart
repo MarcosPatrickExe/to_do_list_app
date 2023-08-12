9ae458d671fc6a11e80a6347e1304c8a8dd15c85
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-08-11 20:25:24 -0600

    Update Activity.dart

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -4,8 +4,8 @@ import 'Member.dart';
 // classe de modelo que sera usada na lista
 class Activity {
        String title;
-       String? description;
-       DateTime? dateRelease;
+       String? description; // dado opcional, podendo receber valor nulo
+       DateTime? dateRelease; // dado opcional, podendo receber valor nulo
        DateTime reminderDate; // data de lembrete
       // Member owner;
        String owner; // o nome do criador (ou owner) ja vai ser definido automaticamente atraves do login do usuario
