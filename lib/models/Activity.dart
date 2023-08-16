be187363d388b42710439267bab8a254a75a874b
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-08-15 22:09:06 -0300

    Update Activity.dart

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -6,7 +6,7 @@ class Activity {
        String title;
        String? description; // dado opcional, podendo receber valor nulo
        DateTime? dateRelease; // dado-horario opcional, podendo receber valor nulo. Referente ao valor de criaçao do lembrete
-       DateTime reminderDate; // data-horario de lembrete
+       DateTime reminderDate; // data-horario de lembrete. Esse ja nao eh opcional, eh obrigatorio
       // Member owner;
        String owner; // o nome do criador (ou owner) ja vai ser definido automaticamente atraves do login do usuario
        bool isFavorite; 
