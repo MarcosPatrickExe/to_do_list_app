cd76d6dae79674e1d7247de563de5b4822ba0c45
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-08-16 23:42:28 -0300

    Update Member.dart

--- lib/models/Member.dart
+++ lib/models/Member.dart
@@ -1,7 +1,7 @@
 
 // essa classe servira para tipar uma List<> na qual ira receber alguns usuarios do github atraves de uma chamada a API
 class Member {
-        String login;     // login basico com nomes e numeros
+        String login;     // login basico com nomes e numeros com padrao alfanumerico
         String avatarUrl; // essa String só funciona caso o link da imagem esteja no formato padrão
 
         Member( this.login, this.avatarUrl ); // construtor modelo sem ser do tipo nomeado e nem do tipo posicional
