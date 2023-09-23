0f0ea880e77affd7bdd92f4b4dccea9ef704f1d4
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-09-22 23:54:14 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
 }
 
 
-// aqui seria uma interface para elementos com 2 propriedades opcionais
+// aqui seria uma interface para elementos com duas propriedades opcionais
 /* class Elements {
         Elements( {this.body, this.footerBottoms } );
 
