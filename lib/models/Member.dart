092f7c5d3c30e837c8da44583bafcb95d13460c1
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-10-02 23:58:20 -0300

    Update Member.dart

--- lib/models/Member.dart
+++ lib/models/Member.dart
@@ -4,5 +4,6 @@ class Member {
         String login;     // login basico com nomes e numeros com padrao alfanumerico
         String avatarUrl; // essa String só funciona caso o link da imagem esteja no formato padrão
 
+        // espaco para separar o construtor do restante das propriedades
         Member( this.login, this.avatarUrl ); // construtor simples sem ser do tipo nomeado e nem do tipo posicional
 }
