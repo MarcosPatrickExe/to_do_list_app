34edf8af15cb2d72e7568fa6f7ab37d6760891a1
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-10-27 23:47:30 -0300

    Update ActivityList.dart

--- lib/pages/ActivityList.dart
+++ lib/pages/ActivityList.dart
@@ -4,7 +4,7 @@ import '../models/Activity.dart';
 import '../pages/FavoritesActivities.dart';
 
 
-
+                        // inserindo statuful
 class ActivityList extends StatefulWidget {
 
             // inserindo modelos de dados, de modo manual, para teste
