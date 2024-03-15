fb99147e21e692f18e0d773cdc962a63b2add7a0
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-03-14 22:18:24 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ import 'pages/Home.dart';
 
 // aqui nesse arquivo 'main' futuramente pdera ser adicionado um BlocProvider
 
-
-
 void main() {
      runApp( const MyApp() );
     // 'runApp' eh um metodo declarado na super-classe que recebe qualquer objeto Widget, ou que estende ele
