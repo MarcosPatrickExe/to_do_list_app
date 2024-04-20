bfc22a5d95612227acbaeffeaf5a13e55073ce22
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-04-19 23:58:01 -0300

    Update Users.dart

--- lib/pages/Users.dart
+++ lib/pages/Users.dart
@@ -28,6 +28,7 @@ class UsersState extends State<Users>{
                   this._loadData();
         }
 
+        // custom function
         void _loadData() async {  // funcoes assincronas sao disparadas por outro processo ao inves da thread principal, nao interferindo na renderizacao da UI
                 String url = "https://api.github.com/orgs/adobe/members";
                 http.Response response =  await  http.get( Uri.parse(url) ); // o 'await' automaticamente faz com que a funcao '_loadData()' espera até receber a resposta da requisicao 
