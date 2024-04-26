46025ceadc9b347d1224d2760a721d23a6a0257c
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-04-25 23:54:55 -0300

    Update Member.dart

--- lib/models/Member.dart
+++ lib/models/Member.dart
@@ -4,5 +4,5 @@ class Member {
         String login;     // valor com padrao alfanumerico
         String avatarUrl; // essa String só funciona caso o link da imagem esteja no formato padrão, ou seja, no formato HTTP
 
-        Member( this.login, this.avatarUrl ); // construtor do tipo nomeado e nem do tipo posicional
+        Member( this.login, this.avatarUrl ); // named constructor type
 }
