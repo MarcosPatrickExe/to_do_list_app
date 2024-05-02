81c6dee0ec7063bd967bced145acc802a2011faf
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-05-01 23:56:39 -0300

    Update Member.dart

--- lib/models/Member.dart
+++ lib/models/Member.dart
@@ -1,6 +1,5 @@
 
 
-
 class Member {
         String login;     // valor com padrao alfanumerico
         String avatarUrl; // essa String só funciona caso o link da imagem esteja no formato padrão, ou seja, no formato HTTP
