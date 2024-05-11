9dcf53d686bcdb69c368ddcdb893ac30b2626895
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-05-10 23:53:55 -0300

    Update Home.dart

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../components/BottomNavBarComp.dart';
 
 
-// ESSA TELA FUTURAMENTE SERÁ SUBSTITUIDA PELA TELA DE LOGIN
+// this screen will be replace by the Login view
 // componente que representa uma pagina com estado dinamico, o qual vai ser gerenciado pela classe "_MyHomePageState"
 class Home extends StatefulWidget {  // widget com estado mutavel
 
