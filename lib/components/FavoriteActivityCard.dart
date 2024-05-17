44abbded7349c8b188d5069f99c4a65e4f7b713e
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-05-17 00:00:37 -0300

    Update FavoriteActivityCard.dart

--- lib/components/FavoriteActivityCard.dart
+++ lib/components/FavoriteActivityCard.dart
@@ -28,7 +28,7 @@ class FavoriteActivityCard extends StatelessWidget{
                                           SizedBox( width: 20.0, ),
                                           Column(
                                                 crossAxisAlignment: CrossAxisAlignment.start, //alinhamento vertical
-                                                children: [ // inserindo um mini card horizontal
+                                                children: [ // inserting a minimal card horizontally
                                                       Container(
                                                             width: 210,
                                                             child: Text( this._activityReceived.title, style: TextStyle( fontSize: 20.0, color: Colors.white), ),
