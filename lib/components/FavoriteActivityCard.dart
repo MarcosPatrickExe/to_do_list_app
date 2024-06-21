27553a864782c86948a5c2d94db2f99f7cd3c506
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-06-20 23:36:26 -0300

    Update FavoriteActivityCard.dart

--- lib/components/FavoriteActivityCard.dart
+++ lib/components/FavoriteActivityCard.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list/pages/FavoritesActivities.dart';
 import '../models/Activity.dart';
 
-  
+    
 
 class FavoriteActivityCard extends StatelessWidget{
 
