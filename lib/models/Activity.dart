c7b812f66228a73af216011f2aff6b7b35599afe
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-07-02 23:49:24 -0300

    Update Activity.dart

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -1,7 +1,7 @@
 import 'Member.dart'; 
 // import basico do Member 
  
-  
+   
 // classe de modelo para o armazenamento das atividades que serao usadas na lista
 class Activity {
        String title; 
