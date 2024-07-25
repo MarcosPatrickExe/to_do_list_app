bbeb0be08a79a63bcdef865d38a04ffc4e824001
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-07-24 23:56:07 -0300

    Update ActivityList.dart

--- lib/pages/ActivityList.dart
+++ lib/pages/ActivityList.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/Activity.dart';
 import '../pages/FavoritesActivities.dart';
- 
+  
                         
 class ActivityList extends StatefulWidget {
   
