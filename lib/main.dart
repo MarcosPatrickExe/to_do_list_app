76894584faf3d1be95d0ca8024c55a6af6516b57
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-08-01 23:56:40 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'pages/Home.dart';
 void main() {        
      runApp( const MyApp() );    
 }    
- 
+  
 class MyApp extends StatelessWidget {  
           const MyApp( { super.key } );   
    
