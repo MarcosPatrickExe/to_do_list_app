57025e23756016b90eab98c9317c86faf3754c92
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2024-08-26 22:58:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'pages/Signup.dart';      
 import 'pages/ActivityList.dart';       
 import 'pages/Home.dart';        
-// export 'pages/Home.dart'; // trying export classes    
+// export 'pages/Home.dart'; // trying export classes     
                    
                  
 void main() {               
