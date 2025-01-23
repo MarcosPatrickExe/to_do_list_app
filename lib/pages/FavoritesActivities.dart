4bbb3941049f2f56c665ba178409e4551e13986f
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-01-23 16:26:12 -0300

    Update FavoritesActivities.dart

--- lib/pages/FavoritesActivities.dart
+++ lib/pages/FavoritesActivities.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart'; 
 import 'package:flutter/material.dart'; 
 import '../models/Activity.dart';    
 import '../components/FavoriteActivityCard.dart';   
