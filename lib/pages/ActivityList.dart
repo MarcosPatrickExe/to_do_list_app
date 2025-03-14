038ecfa228ad5fc30cf5b35b3e865785f2ac0b1a
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-03-14 09:40:00 -0300

    Update ActivityList.dart

--- lib/pages/ActivityList.dart
+++ lib/pages/ActivityList.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';  
 import '../models/Activity.dart';
 import '../pages/FavoritesActivities.dart';
    
