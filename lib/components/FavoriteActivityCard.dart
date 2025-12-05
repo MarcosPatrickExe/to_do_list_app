7543839506c9d9b745f65321fdc4e08a53b964f0
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2025-12-04 23:07:00 -0300

    Update FavoriteActivityCard.dart

--- lib/components/FavoriteActivityCard.dart
+++ lib/components/FavoriteActivityCard.dart
@@ -14,7 +14,7 @@ class FavoriteActivityCard extends StatelessWidget{
       @override
       Widget build( BuildContext  favoriteActivitiesContext ){
             return Card(
-                      margin: const EdgeInsets.all(10.0), // many paddings
+                      margin: const EdgeInsets.all(10.0), 
                       color: const Color.fromARGB(175, 87,  5,  110),  
                       shape: const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all( Radius.circular(20.0) ) // aplicando a borda redonda em todos os vertices (ou edges)
