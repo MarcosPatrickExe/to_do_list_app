f4a94b3246da4c3a3bf8ec7bfaffe9ccd4a12085
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-02-03 19:01:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'pages/Signup.dart';
 import 'pages/ActivityList.dart';                                             
 import 'pages/Home.dart';   
 
-                               
+
+
 void Builder(){ 
     println("test my connection!");  
     println("default conectionnn!!! ");
