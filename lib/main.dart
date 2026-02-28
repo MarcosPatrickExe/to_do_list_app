f330405ff58eaa68d39a6754a377fc1df2e4ca17
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-02-27 22:59:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'pages/Home.dart';
 void Builder(){ 
     println("test my connection! ");  
     println("default conectionnn!! ");
-    println("my third connection simulation! ");
+    println(" my third connection simulation! ");
     _CratorFrom();  
 }
 
