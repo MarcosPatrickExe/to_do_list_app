3f6616af1baa9b8fc3153e2459bab6c16831b5ad
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-04-01 21:57:15 -0300

    Update Member.dart

--- lib/models/Member.dart
+++ lib/models/Member.dart
@@ -1,5 +1,5 @@
  class Member {    
-   String login;            
+   String login;   
    String avatarUrl;  // link in HTTP format    
    
    Member( this.login, this.avatarUrl );    // member of class 
