36df5e3e80760be9eca3156bf6ae170884fbb04c
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-04-17 23:01:48 -0300

    Update Signup.dart

--- lib/pages/Signup.dart
+++ lib/pages/Signup.dart
@@ -14,6 +14,7 @@ class Fields {  // equivalente a uma interface
 }
 
 
+
 // essa tela somente será acessada na tela de login como uma opção onde o usuário poderá clicar para se cadastrar
 class Signup extends StatelessWidget {
         const Signup( {super.key} );
