acd9a9514268c513debb46b249e76436ea8589bc
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2026-05-26 23:25:26 -0300

    Update FavoritesActivities.dart

--- lib/pages/FavoritesActivities.dart
+++ lib/pages/FavoritesActivities.dart
@@ -8,7 +8,7 @@ class FavoriteActivities extends StatefulWidget {
 
   // propriedades que serao inicializadas mais tarde na clsse 'FavoriteActivityState'
   late List<Activity> activitiesList;
-        late Function changeList;
+  late Function changeList;
         FavoriteActivities(this.activitiesList, this.changeList, {super.key});// construtor padrao
         
         @override 
